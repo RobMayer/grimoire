5c8b7b37a2536c635036f814dfa73f831f4e1a6e
Rob Mayer <5171361+RobMayer@users.noreply.github.com>
2024-08-09 16:55:53 -0400

    proper layout

--- src/books/sample/index.tsx
+++ src/books/sample/index.tsx
@@ -9,7 +9,6 @@ const Sample = async () => {
     return (
         <html>
             <head>
-                <style>{resetCSS}</style>
                 <style>{styles}</style>
                 {/* <script src={"https://unpkg.com/pagedjs@0.5.0-beta.1/dist/paged.polyfill.min.js"}></script> */}
                 <title>Style Document Test</title>
@@ -21,71 +20,108 @@ const Sample = async () => {
                     <address>Author</address>
                     <div className="version">v1.0</div>
                 </hgroup>
+                <nav>
+                    <h2>Tabele of Contents</h2>
+                    <ul>
+                        <li>
+                            <a href="#">Ch Ref</a>
+                        </li>
+                        <li>
+                            <a href="#">Sec Ref</a>
+                        </li>
+                    </ul>
+                </nav>
+                <nav>
+                    <h2>Table of Figures</h2>
+                    <ul>
+                        <li>
+                            <a href="#">Ch Ref</a>
+                        </li>
+                        <li>
+                            <a href="#">Fig Ref</a>
+                        </li>
+                    </ul>
+                </nav>
                 <header>
-                    <nav>
-                        <h2>Tabele of Contents</h2>
-                        <ul>
-                            <li>
-                                <a href="#">Chapter 1</a>
-                            </li>
-                            <li>
-                                <a href="#">I.1: Section 1</a>
-                            </li>
-                        </ul>
-                    </nav>
-                    <nav>
-                        <h2>Table of Figures</h2>
-                        <ul>
-                            <li>
-                                <a href="#">I: Chapter 1</a>
-                            </li>
-                            <li>
-                                <a href="#">I.1: Figure 1</a>
-                            </li>
-                        </ul>
-                    </nav>
-                    <article>
-                        <h2>Preface or Intro</h2>
-                    </article>
+                    <h2>Preface or Intro</h2>
+                    <p>Paragraph</p>
                 </header>
                 <main>
-                    <article>
-                        <h2>Chapter 1</h2>
+                    <h2>Chapter 1</h2>
+                    <p>Paragraph</p>
+                    <section>
+                        <h3>Section 1</h3>
                         <p>Paragraph</p>
-                        <section>
-                            <h3>Section 1</h3>
-                            <p>Paragraph</p>
-                        </section>
-                    </article>
-                    <article>
-                        <h2>Chapter 2</h2>
-                        <p>Paragraph</p>
-                        <section>
-                            <h3>Section 1</h3>
-                            <p>Paragraph</p>
-                        </section>
-                    </article>
+                    </section>
                 </main>
-                <footer>
-                    <nav>
-                        <h2>Index</h2>
-                        <ul>
-                            <li>
-                                <a href="#">Some Text</a>
-                            </li>
-                        </ul>
-                    </nav>
-                    <nav>
-                        <h2>Glossary</h2>
-                        <dl>
-                            <dt>Word</dt>
-                            <dd>Definition</dd>
-                        </dl>
-                    </nav>
-                    <article>
-                        <h2>Aftwrward</h2>
+                <main>
+                    <h2>Chapter 2</h2>
+                    <p>Paragraph</p>
+                    <section>
+                        <h3>Section 1</h3>
                         <p>Paragraph</p>
-                    </article>
+                    </section>
+                    <section>
+                        <h3>Section 2</h3>
+                        <p>Paragraph</p>
+                        <figure>
+                            <figcaption>Some Image</figcaption>
+                        </figure>
+
+                        <table>
+                            <caption>Some Table</caption>
+                            <thead>
+                                <tr>
+                                    <th>Column 1</th>
+                                    <th>Column 2</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1,1</td>
+                                    <td>2,1</td>
+                                </tr>
+                                <tr>
+                                    <td>1,2</td>
+                                    <td>2,2</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </section>
+                </main>
+                <aside>
+                    <h2>Interlude</h2>
+                </aside>
+                <main>
+                    <h2>Chapter 3</h2>
+                    <p>Paragraph</p>
+                    <section>
+                        <h3>Section 1</h3>
+                        <p>Paragraph</p>
+                    </section>
+                    <section>
+                        <h3>Section 2</h3>
+                        <p>Paragraph</p>
+                    </section>
+                </main>
+                <nav>
+                    <h2>Index</h2>
+                    <ul>
+                        <li>
+                            <a href="#">Some Text</a>
+                        </li>
+                    </ul>
+                </nav>
+                <nav>
+                    <h2>Glossary</h2>
+                    <dl>
+                        <dt>Word</dt>
+                        <dd>Definition</dd>
+                    </dl>
+                </nav>
+                <footer>
+                    <h2>Aftwrward</h2>
+                    <p>Paragraph</p>
                 </footer>
             </body>
         </html>
